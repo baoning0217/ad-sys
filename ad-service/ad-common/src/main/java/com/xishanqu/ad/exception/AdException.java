32c94751ff51d211b298c291ae7dfdf18c5e196b
baoning <bnwyyx217@163.com>
2019-04-09 14:17:03 +0800

    init

---
+++ ad-service/ad-common/src/main/java/com/xishanqu/ad/exception/AdException.java
@@ -0,0 +1,12 @@
+package com.xishanqu.ad.exception;
+
+/**
+ * @Author BaoNing 2019/4/2
+ */
+public class AdException extends Exception {
+
+    public AdException(String message){
+        super(message);
+    }
+
+}
